6bc600bad85574a2bb5adc0d6369303a2e90719d
WillRMorris <153463826+WillRMorris@users.noreply.github.com>
2024-02-01 16:42:09 -0700

    made icons appear appropriately for both today and on the five day forcast (see not in handle5Day function) also added responsiveness for medium screen sizes, and touched up styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var lon;
 
 function fetchForcast(){
 
-    fetch('https://api.openweathermap.org/geo/1.0/direct?q=Tokyo&limit=1&appid=a9909a5368cc548ae8f89a889fbd85c9')
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=Salt Lake City&limit=1&appid=a9909a5368cc548ae8f89a889fbd85c9')
     .then((res) => res.json())
     .then(place = function(data) {
         var place = data[0];
@@ -16,14 +16,7 @@ function fetchForcast(){
         .then((res) => res.json())
         .then(function(data){
             var weather = data;
-            let place = weather.name;
-            console.log();
-            let temp = weather.main.temp;
-            let wind = weather.wind.speed;
-            let hum = weather.main.humidity;
-            let icon = weather.weather[0].icon;
-            console.log(weather);
-            renderToday(place, temp, wind, hum, icon);
+            renderToday(weather);
         });
 
         //5 days
@@ -39,17 +32,28 @@ function fetchForcast(){
 };
 
 //renders today im html given date, temp, wind, nad hum
-function renderToday (place, temp, wind, hum, icon) {
+function renderToday (weather) {
+    let place = weather.name;
+    let temp = weather.main.temp;
+    let wind = weather.wind.speed;
+    let hum = weather.main.humidity;
+    let icon = weather.weather[0].icon;
+    let alt = weather.weather[0].description;
+
     var wrapper = $('#today');
     var dateBlock = $('#today-date');
+    var image = $('#today-icon');
     var tempBlock = $('#today-temp');
     var windBlock = $('#today-wind');
     var humBlock = $('#today-humid');
+    image.attr(`src` , `https://openweathermap.org/img/wn/${icon}.png`);
+    image.attr(`alt` , `${alt}`);
+
 
     dateBlock.text(`${place}, Right Now `);
     tempBlock.text( `Temp: ${temp}℉`);
-    windBlock.text(`Wind: ${wind} mph`);
-    humBlock.text(`Humidity: ${hum} %`);
+    windBlock.text(`Wind: ${wind}mph`);
+    humBlock.text(`Humidity: ${hum}%`);
 
     wrapper.css('display', 'flex');
 
@@ -67,37 +71,50 @@ function handle5Day (weather) {
                 //only triggers when the day changes including the first day
                 if(currentDate != tempDate){
                     currentDate = tempDate;
+                    console.log(currentDate)
 
                     //dates should change at midnight 0:00:00 intervals are in 3 hours, so four items ahead of the date change should be noon(12:00:00), which I think is a good time to give a general forcast for a future date as opposed to the temp at midnight.
                     var noon = i+4;
                     //checks to make sure that there is an object at index noon. the first and last items, depending on the time of day may not have an item, or that item may not be noon. If the var noon does not have a date equal to what noon on the current day should look like, creates an object with the current (so earliest avaible) time on that day. Not ideal, but should safely give a forcast for each day.
-                    if(list[noon].dt_txt === `${currentDate} 12:00:00`){
+
+                    //note: for whatever reason, OpenWeather considers 12:00:00 (noon) to be at night. Thus even with a sucessful call of noon, we still get the nighttime icons.
+                    if(list[noon].dt_txt == `${currentDate} 12:00:00`){
                         let temp = list[noon].main.temp;
                         let wind = list[noon].wind.speed;
                         let hum = list[noon].main.humidity;
-                        renderDay(currentDate, temp, wind, hum )
+                        let icon = list[noon].weather[0].icon;
+                        let alt = list[noon].weather[0].description;
+                        console.log("yes")
+                        renderDay(currentDate, temp, wind, hum, icon, alt )
                     } else{
                         let temp = list[i].main.temp;
                         let wind = list[i].wind.speed;
                         let hum = list[i].main.humidity;
-                        renderDay(currentDate, temp, wind, hum )
+                        let icon = list[i].weather[0].icon;
+                        let alt = list[i].weather[0].description;
+                        console.log('no')
+
+                        renderDay(currentDate, temp, wind, hum, icon, alt )
                     }
                 }
             }
 }
 
 //renders a day on the 5-day on the HTML given variables of date, temp, wind, hum
-function renderDay (date, temp, wind, hum) {
+function renderDay (date, temp, wind, hum, icon, alt) {
     var wrapper = $('#five-day-wrapper');
-    var card = $('<div class = "day-card card flex-wrap m-1 col-12 col-md-2 justify-content-between border px-1 py-3"> </div>');
+    var card = $('<div class = "day-card card rounded flex-wrap col-12 col-md-12 col-lg-2 justify-content-between border px-1 py-3 mx-0 my-3 my-lg-0"> </div>');
     var dateBlock = $('<div class = "card-title h6">');
-    var list = $('<ul class= "mr-1 my-2 list-unstyled small-font-size">');
+    var imgWrapper = $('<div class="my-2">')
+    var img = $(`<img src = "https://openweathermap.org/img/wn/${icon}.png" alt = "${alt}" class = fiveDayIcon>`);
+    var list = $('<ul class= "mr-1 my-2 list-unstyled">');
     var tempBlock = $('<li>');
     var windBlock = $('<li>');
     var humBlock = $('<li>');
-
     wrapper.append(card);
     card.append(dateBlock);
+    card.append(imgWrapper);
+    imgWrapper.append(img);
     card.append(list);
     list.append(tempBlock);
     list.append(windBlock);
@@ -105,8 +122,8 @@ function renderDay (date, temp, wind, hum) {
 
     dateBlock.text(`${date}`);
     tempBlock.text( `Temp: ${temp}℉`);
-    windBlock.text(`Wind: ${wind} mph`);
-    humBlock.text(`Humidity: ${hum} %`);
+    windBlock.text(`Wind: ${wind}mph`);
+    humBlock.text(`Humidity: ${hum}%`);
 
     
 }

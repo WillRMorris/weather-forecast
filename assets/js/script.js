2ba2f26670a23c573837e02a93058e9a2fbc8193
WillRMorris <153463826+WillRMorris@users.noreply.github.com>
2024-02-01 17:32:37 -0700

    made the intial geo fetch take a variable. cleaned up a few potential errors based on the time fetched at, limiting the amount of days to 5, and creating an if statment to catch if the noon var would be out of the array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
 //℉ for when we need fareheit
-var lat;
-var lon;
 
-function fetchForcast(){
+function fetchForcast(location){
 
-    fetch('https://api.openweathermap.org/geo/1.0/direct?q=Salt Lake City&limit=1&appid=a9909a5368cc548ae8f89a889fbd85c9')
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=a9909a5368cc548ae8f89a889fbd85c9`)
     .then((res) => res.json())
     .then(place = function(data) {
         var place = data[0];
-        lat = place.lat;
-        lon= place.lon;
+       var lat = place.lat;
+        var lon= place.lon;
         console.log(place);
         //today
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=a9909a5368cc548ae8f89a889fbd85c9`)
         .then((res) => res.json())
         .then(function(data){
             var weather = data;
+            console.log(weather);
             renderToday(weather);
         });
 
@@ -68,34 +67,46 @@ function handle5Day (weather) {
             var currentDate = '';
             for(var i = 0; i<list.length; i++){
                 tempDate = list[i].dt_txt.slice(0, 10);
+                var blockCount = 0;
                 //only triggers when the day changes including the first day
                 if(currentDate != tempDate){
                     currentDate = tempDate;
-                    console.log(currentDate)
 
                     //dates should change at midnight 0:00:00 intervals are in 3 hours, so four items ahead of the date change should be noon(12:00:00), which I think is a good time to give a general forcast for a future date as opposed to the temp at midnight.
-                    var noon = i+4;
+                    var noon;
+                    if(i+4 > list.length){
+                        noon = 0;
+                    }else{
+                        noon = i+4; 
+                    }
                     //checks to make sure that there is an object at index noon. the first and last items, depending on the time of day may not have an item, or that item may not be noon. If the var noon does not have a date equal to what noon on the current day should look like, creates an object with the current (so earliest avaible) time on that day. Not ideal, but should safely give a forcast for each day.
+                    //UPDATE: does in fact throw an error, though the program runs regardless. In fact apprently, the way the system works, we have the start of a day 6 in our 5 day forcast at certain times during the day.
 
-                    //note: for whatever reason, OpenWeather considers 12:00:00 (noon) to be at night. Thus even with a sucessful call of noon, we still get the nighttime icons.
-                    if(list[noon].dt_txt == `${currentDate} 12:00:00`){
+                    //NOTE: for whatever reason, OpenWeather considers 12:00:00 (noon) to be at night. Thus even with a sucessful call of noon, we still get the nighttime icons.
+                    if(list[noon].dt_txt ==`${currentDate} 12:00:00`){
                         let temp = list[noon].main.temp;
                         let wind = list[noon].wind.speed;
                         let hum = list[noon].main.humidity;
                         let icon = list[noon].weather[0].icon;
                         let alt = list[noon].weather[0].description;
-                        console.log("yes")
-                        renderDay(currentDate, temp, wind, hum, icon, alt )
+                        console.log("noon forcast"+ i);
+                        renderDay(currentDate, temp, wind, hum, icon, alt );
                     } else{
                         let temp = list[i].main.temp;
                         let wind = list[i].wind.speed;
                         let hum = list[i].main.humidity;
                         let icon = list[i].weather[0].icon;
                         let alt = list[i].weather[0].description;
-                        console.log('no')
+                        console.log('earliest forcast'+ i);
 
-                        renderDay(currentDate, temp, wind, hum, icon, alt )
+                        renderDay(currentDate, temp, wind, hum, icon, alt );
                     }
+                    if( blockCount< 5){
+                        blockCount++;
+                    }else{
+                        return;
+                    }
+
                 }
             }
 }
@@ -103,6 +114,7 @@ function handle5Day (weather) {
 //renders a day on the 5-day on the HTML given variables of date, temp, wind, hum
 function renderDay (date, temp, wind, hum, icon, alt) {
     var wrapper = $('#five-day-wrapper');
+    var headingWrapper = $('fiveDay-heading-wrapper');
     var card = $('<div class = "day-card card rounded flex-wrap col-12 col-md-12 col-lg-2 justify-content-between border px-1 py-3 mx-0 my-3 my-lg-0"> </div>');
     var dateBlock = $('<div class = "card-title h6">');
     var imgWrapper = $('<div class="my-2">')
@@ -111,6 +123,9 @@ function renderDay (date, temp, wind, hum, icon, alt) {
     var tempBlock = $('<li>');
     var windBlock = $('<li>');
     var humBlock = $('<li>');
+
+    headingWrapper.css('display' , 'flex');
+
     wrapper.append(card);
     card.append(dateBlock);
     card.append(imgWrapper);
@@ -128,5 +143,12 @@ function renderDay (date, temp, wind, hum, icon, alt) {
     
 }
 
-fetchForcast();
+function clear5Day(){
+    var fiveDayForcast = $('#five-day-wrapper');
+    fiveDayForcast.empty();
+
+    var headingWrapper = $('fiveDay-heading-wrapper');
+    headingWrapper.css('display' , 'none');
+}
+fetchForcast('Salt Lake City');
 
